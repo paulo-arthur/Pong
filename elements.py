b35a55e1d51ea8b2ddac63531db922227e1ad81f
Paulo Arthur <pauloart.hur670@gmail.com>
2022-08-06 20:25:21 -0300

    adding the files

--- elements.py
+++ elements.py
@@ -1,9 +1,9 @@
-import math
+import math, random
 
 def bar(WIDTH, HEIGHT, x):
     return {
-    'length': 100,
-    'weight': 7,
+    'length': 70,
+    'weight': 5,
     'x': x,
     'y': HEIGHT/2
     }
@@ -13,26 +13,27 @@ def ball(WIDTH, HEIGHT):
     'radius': 10,
     'x': WIDTH/2,
     'y': HEIGHT/2,
-    'speed': [0.6, 0]
+    'speed': [0.4, random.randint(2, 5)/10],
+    'xlr8': 0.00001
     }
 
 
-def collision(rect_left, rect_top, width, height, center_x, center_y, radius):
+def collision(rleft, rtop, width, height, center_x, center_y, radius):
 
-    rect_right, rect_bottom = rect_left + width/2, rect_top + height/2
+    rright, rbottom = rleft + width, rtop + height
 
-    cleft, ctop     = center_x-radius, center_y-radius
-    cright, cbottom = center_x+radius, center_y+radius
+    cleft, ctop     = center_x - radius, center_y - radius
+    cright, cbottom = center_x + radius, center_y + radius
 
-    if rect_right < cleft or rect_left > cright or rect_bottom < ctop or rect_top > cbottom:
+    if rright < cleft or rleft > cright or rbottom < ctop or rtop > cbottom:
         return False
 
-    for x in (rect_left, rect_left+width):
-        for y in (rect_top, rect_top+height):
-            if math.hypot(x-center_x, y-center_y) <= radius:
+    for x in (rleft, rleft+width):
+        for y in (rtop, rtop+height):
+            if math.hypot(x-center_x, y - center_y) <= radius:
                 return True
 
-    if rect_left <= center_x <= rect_right and rect_top <= center_y <= rect_bottom:
+    if rleft <= center_x <= rright and rtop <= center_y <= rbottom:
         return True
 
     return False
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, sys, random
 from elements import bar, ball, collision
 
 pygame.init()
+pygame.font.get_init()
 
 pygame.display.set_caption('Pong')
 
@@ -10,14 +11,16 @@ HEIGHT = 600
 BLACK = 40, 40, 40
 WHITE = 220, 220, 220
 
-L_Bar = bar(WIDTH, HEIGHT, 30)
-R_Bar = bar(WIDTH, HEIGHT, WIDTH - 30)
+L_Bar = bar(WIDTH, HEIGHT, 50)
+R_Bar = bar(WIDTH, HEIGHT, WIDTH - 50)
 
 ball = ball(WIDTH, HEIGHT)
 
+counter = 0
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-while True:
+while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -36,6 +39,9 @@ while True:
     #background
     screen.fill(BLACK)
 
+    #counter
+    font = pygame.font.SysFont(None, 50).render(str(counter), True, WHITE)
+
     #bars
     pygame.draw.rect(screen, WHITE, pygame.Rect(L_Bar['x'], L_Bar['y'], L_Bar['weight'], L_Bar['length']))
     pygame.draw.rect(screen, WHITE, pygame.Rect(R_Bar['x'], R_Bar['y'], R_Bar['weight'], R_Bar['length']))
@@ -44,12 +50,30 @@ while True:
     pygame.draw.circle(screen, WHITE, (ball['x'], ball['y']), ball['radius'])
     ball['x'] += ball['speed'][0]
     ball['y'] += ball['speed'][1]
+    ball['speed'][0] += ball['xlr8'] * ball['speed'][0]/abs(ball['speed'][0])
 
-    #check collision
-    if collision(L_Bar['x'], L_Bar['y'], 2*L_Bar['weight'] + ball['radius'], L_Bar['length'], ball['x'], ball['y'], ball['radius']):
-        ball['speed'][0] *= -1
-    if collision(R_Bar['x'], R_Bar['y'], R_Bar['weight'], R_Bar['length'], ball['x'], ball['y'], ball['radius']):
-        ball['speed'][0] *= -1
+    #check BALL-BAR collision
+    if ball['x'] < 0.2 * WIDTH:
+        if collision(L_Bar['x'], L_Bar['y'], L_Bar['weight'], L_Bar['length'], ball['x'], ball['y'], ball['radius']):
+            ball['speed'][0] *= -1
+            ball['speed'][1] *= random.randint(5, 15)/10
+            counter += 1
+    if ball['x'] > 0.8 * WIDTH:
+        if collision(R_Bar['x'], R_Bar['y'], R_Bar['weight'], R_Bar['length'], ball['x'], ball['y'], ball['radius']):
+            ball['speed'][0] *= -1
+            ball['speed'][1] *= random.randint(5, 15)/10
+            counter += 1
 
-    pygame.display.flip()
+    #check BALL-WALL collision
+    if ball['y'] <= ball['radius']:
+        ball['speed'][1] *= -1
+    if ball['y'] >= HEIGHT - ball['radius']:
+        ball['speed'][1] *= -1
+
+    #game over
+    if ball['x'] < 0 or ball['x'] > WIDTH:
+        break
+
+    screen.blit(font, (20,20))
     pygame.display.update()
+input()
